7e3c03b0dcb4ce50f6f000e2e620c82536f6ef56
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2022-02-11 12:34:48 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LINKTOENTITY.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<title>Bind Gridview with LINQ to ADO.NET Operations</title>
+<style type="text/css">
+.GridviewDiv {font-size: 100%; font-family: 'Lucida Grande', 'Lucida Sans Unicode', Verdana, Arial, Helevetica, sans-serif; color: #303933;}
+.headerstyle
+{
+color:#FFFFFF;border-right-color:#abb079;border-bottom-color:#abb079;background-color: #df5015;padding:0.5em 0.5em 0.5em 0.5em;text-align:center;
+}
+</style>
+</head>
+<body>
+<form id="form1" runat="server">
+<div class="GridviewDiv">
+<asp:GridView ID="gvDetails" CssClass="Gridview" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="gvDetails_SelectedIndexChanged">
+<HeaderStyle CssClass="headerstyle" />
+<Columns>
+<asp:BoundField HeaderText="Name" DataField="Name" />
+<asp:BoundField HeaderText="Location" DataField="Location" />
+<asp:BoundField HeaderText="Gender" DataField="Gender" />
+</Columns>
+</asp:GridView>
+</div>
+</form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINKTOENTITY
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvDetails_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindGridData();
+            }
+        }
+        private void BindGridData()
+        {
+            // Create the object of the Context defined in the constructor of the context class.
+            vishalEntities1 db = new vishalEntities1();
+            var result = from emp in db.EmployeeDetails
+                         select new
+                         {
+                             Id = emp.EmpId,
+                             Name = emp.EmpName,
+                             Location = emp.Location,
+                             Gender = emp.Gender
+                         };
+            GridView.DataSource = result.ToList();
+            GridView.DataBind();
+        }
+    }
+}
